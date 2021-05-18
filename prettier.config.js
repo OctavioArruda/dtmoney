18b9be1605ca3d5bb62d321159b0ff6671047074
OctavioArruda <octavio.arruda@poatek.com>
2021-05-17 22:30:13 -0300

    editorconfig+eslint+prettier

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  singleQuote: true,
+  arrowParens: 'avoid',
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: true,
+  semi: true,
+  trailingComma: "es5",
+  bracketSpacing: true
+}
