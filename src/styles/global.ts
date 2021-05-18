70941ff7731bb98b434776ea022b78905dca3cf7
OctavioArruda <octavio.arruda@poatek.com>
2021-05-18 08:31:29 -0300

    Added responsive breakpoints and global styles

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -2,7 +2,14 @@ import { createGlobalStyle } from 'styled-components'
 
 export const GlobalStyle = createGlobalStyle`
   :root {
-    --background: #f0f2f5;
+    --background: #F0F2F5;
+    --red: #E52E4D;
+    --blue: #5429CC;
+    --blue-light: #6933FF;
+    --text-title: #363F5F;
+    --text-body: #969CB3;
+    --background: #F0F2F5;
+    --shape: #FFFFFF;
   }
 
   * {
@@ -11,8 +18,27 @@ export const GlobalStyle = createGlobalStyle`
     box-sizing: border-box;
   }
 
+  html {
+    @media (max-width: 1080px) {
+      font-size: 93.75%;
+    }
+
+    @media (max-width: 720px) {
+      font-size: 87.5%;
+    }
+  }
+
   body {
     background: var(--background);
     -webkit-font-smothing: antialised;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  [disabled] {
+    opacity: 0.6;
+    cursor: not-allowed;
   }
 `
