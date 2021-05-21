29495992564be22acb1313fff38b9df1b52728f8
OctavioArruda <octavio.arruda@poatek.com>
2021-05-21 08:46:49 -0300

    added axios

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,6 +1,14 @@
+import { useEffect } from 'react'
 import { Container } from './styles'
+import { api } from '../../services/api'
 
 export function TransactionsTable(): JSX.Element {
+  useEffect(() => {
+    api
+      .get('/transactions')
+      .then(response => response.data)
+  }, [])
+
   return (
     <Container>
       <table>
