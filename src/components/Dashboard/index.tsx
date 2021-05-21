6abe8a5ba276f51b9baef6eade586eb95d4e5ae0
OctavioArruda <octavio.arruda@poatek.com>
2021-05-21 08:12:49 -0300

    TransactionsTable component

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,10 +1,12 @@
 import { Summary } from '../Summary'
+import { TransactionsTable } from '../TransactionsTable'
 import { Container } from './styles'
 
 export function Dashboard(): JSX.Element {
   return (
     <Container>
       <Summary />
+      <TransactionsTable />
     </Container>
   )
 }
